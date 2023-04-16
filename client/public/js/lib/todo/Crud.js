25f9fd45989af046dcbe4ccedd63a5f88ec644f9
中島浩二 <knaka0209@gmail.com>
2023-04-16 13:13:14 +0900

    up,0416a7

--- client/public/js/lib/todo/Crud.js
+++ client/public/js/lib/todo/Crud.js
@@ -10,14 +10,42 @@ const Crud = {
   {
     try {   
 console.log("#getList");    
-console.log(obj);    
+//console.log(obj);    
       const response = await fetch(obj.url+ "/todos/index");
       const json = await response.json();
       return json;
-//      return [];
     } catch (e) {
       console.error(e);
     }
-  } 
+  },
+  /**
+   * 
+   * @param
+   *
+   * @return
+   */    
+  search : async function(obj)
+  {
+    try {   
+//console.log("#search");    
+      const searchKey = document.querySelector('#searchKey');
+      const item = { search_key: searchKey.value}
+      const body = JSON.stringify(item);		
+      const res = await fetch(obj.url + "/todos/search", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},      
+        body: body
+      });
+      const json = await res.json()
+      if (res.status != 200) {
+        throw new Error(await res.text());
+      }
+//console.log(json);
+      return json;
+    } catch (e) {
+      console.error(e);
+    }
+  },
+
 }
 
